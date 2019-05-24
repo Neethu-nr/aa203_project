98e0e990e38ebb61baef4798dd4f28e68c41eaf5
AlbinJagesten <Albin.Jagesten@gmail.com>
2019-05-24 07:53:17 -0700

    Updated interpolation

--- find_coeff.m
+++ find_coeff.m
@@ -1,26 +1,14 @@
 function [CL, CD] = find_coeff(alpha)
+
+    %Within constraints
+    assert(alpha >= 25 && alpha >= 0)
     alpha = alpha * 180 /pi;
-    if (alpha > 25)
-        CL = NaN;
-        CD = NaN;
-    elseif (alpha > 20)
-        CL = 1.596;
-        CD = 0.583;
-    elseif (alpha > 15)
-        CL = 1.376;
-        CD = 0.365;
-    elseif (alpha > 10)
-        CL = 1.102;
-        CD = 0.184;
-    elseif (alpha > 5)
-        CL = 0.747;
-        CD = 0.082;
-    elseif (alpha > 0)
-        CL = 0.365;
-        CD = 0.039;
-    else
-        CL = NaN;
-        CD = NaN;
-    end
+    
+    table_alpha = [0 5 10 15 20 25];
+    table_CD = [0.049 0.039 0.082 0.184 0.365 0.583];
+    table_CL = [0.025 0.365 0.747 1.102 1.376 1.596];
+    
+    CD = spline(table_alpha, table_CD, alpha);
+    CL = spline(table_alpha, table_CL, alpha);
             
 end
