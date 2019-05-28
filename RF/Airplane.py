d5ca5fb3d11e6a133bfa1bf2b5fbcbb2166350ba
AlbinJagesten <Albin.Jagesten@gmail.com>
2019-05-27 18:49:10 -0700

    Added RF files

---
+++ RF/Airplane.py
@@ -0,0 +1,130 @@
+import numpy as np
+import math
+from scipy.interpolate import interp1d
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+
+class Airplane_Env:
+
+    def __init__(self):
+        self.W = 9200.0
+        self.Surface_area =27.87
+        self.g = 9.81
+        self.rho_air = 1.225
+        self.tau = 2.0  # seconds between state updates
+
+        # x,y and h at which to fail the episode
+        self.h_threshold = 0
+        self.x_threshold = -1000
+        self.y_threshold = 2500
+        self.goal = [5000,0]
+        self.traj = np.array([])
+
+        #x,y,h,V,gamma,chi
+        self.init_pos = np.array([0.0,0.0,10000.0,100.0,0.26,np.pi], dtype=np.float32)
+        self.reset()
+        self.old_alpha = 0
+        self.old_mu = 0
+        #self.observation_space = spaces.Box(-high, high, dtype=np.float32)
+
+
+    def step(self, action):
+
+        alpha,mu = self.action_to_control(action)
+
+        self.old_state = self.state
+        
+        dt = 0.5
+
+        #for i in range(int(self.tau/dt)):
+        #    self.state = self.state + dt * self.dynamics(self.state,alpha,mu)
+        self.state = self.state + self.tau * self.dynamics(self.state,alpha,mu)
+        self.traj = np.append(self.traj,[self.state],axis=0)
+        
+        done = (self.state[2] <= 0) #or self.state[0] <= self.x_threshold or abs(self.state[1]) > self.y_threshold)
+        done = bool(done)
+
+        #dist_to_goal = np.linalg.norm(self.goal-self.state[0:2])
+        #original_dist = np.linalg.norm(self.goal-self.init_pos[0:2])
+
+        #reward = 1000 * (1 - dist_to_goal/original_dist) - ((self.old_alpha - alpha) ** 2 + (self.old_mu - mu) ** 2)
+        reward = self.state[0]# - abs(self.state[1])
+
+        self.old_alpha = alpha
+        self.old_mu = mu
+
+        return np.array(self.state), reward, done, {}
+
+    def action_to_control(self,action):
+        assert(action >= 0 and action < 42)
+
+        alpha_list = np.array([0,5,10,15,20,25]) / 180.0 * np.pi
+        mu_list = np.array([-90,-60,-30,0,30,60,90]) / 180.0 * np.pi
+
+        #alpha_idx * mu_idx
+        alpha_idx = action / 7
+        mu_idx = action % 7
+
+        alpha = alpha_list[alpha_idx]
+        mu = mu_list[mu_idx]
+
+        return alpha,mu
+
+    def reset(self):
+        self.state = self.init_pos
+        self.Vw = 10.0
+        self.chiw = 0.0
+        self.traj = np.array([self.state])
+        return np.array(self.state, dtype=np.float32)
+
+    def dynamics(self,state,alpha,mu):
+        x,y,h,V,gamma,chi = state
+        
+        qS = 0.5 * self.rho_air * V*V * self.Surface_area
+        CL, CD = self.find_coeff(alpha)
+        L = CL * qS
+        D = CD * qS
+        
+        xdot = V*np.cos(gamma)*np.cos(chi) + self.Vw*np.cos(self.chiw)
+        ydot = V*np.cos(gamma)*np.sin(chi) + self.Vw*np.sin(self.chiw)
+        hdot = V*np.sin(gamma)
+        Vdot = -self.g/self.W*(D + self.W*np.sin(gamma))
+        gammadot = self.g/V*(L/self.W*np.cos(mu) - np.cos(gamma))
+        chidot = self.g*L*np.sin(mu) / (V*np.cos(gamma)*self.W)
+
+        return np.array([xdot,ydot,hdot,Vdot,gammadot,chidot], dtype=np.float32)
+
+    def find_coeff(self,alpha):
+
+        alpha = alpha * 180.0 / np.pi
+        
+        table_alpha = [0.0,5.0,10.0,15.0,20.0,25.0]
+        table_CD = [0.049,0.039,0.082,0.184,0.365,0.583]
+        table_CL = [0.025,0.365,0.747,1.102,1.376,1.596]
+        
+        CD = interp1d(table_alpha, table_CD,kind='cubic',fill_value="extrapolate")
+        CL = interp1d(table_alpha, table_CL,kind='cubic',fill_value="extrapolate")
+            
+        return CL(alpha),CD(alpha)
+
+    def get_traj(self):
+        return self.traj
+
+    def plot_traj(self):
+        fig = plt.figure()
+        ax = plt.axes(projection='3d')
+
+        traj = self.get_traj()
+        zline = traj[:,2]
+        xline = traj[:,0]
+        yline = traj[:,1]
+        ax.plot3D(xline, yline, zline, 'gray')
+
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.show()
+
+if __name__ == "__main__":
+    a = Airplane_Env()
+    for i in range(78):
+        print(a.action_to_control(i))
---
+++ RF/DQNAgent.py
@@ -0,0 +1,61 @@
+#From https://github.com/the-deep-learners/TensorFlow-LiveLessons/
+#modified by Albin
+
+import random
+import numpy as np
+from collections import deque
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+
+class DQNAgent:
+    def __init__(self, state_size, action_size, epsilon=1.0):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.memory = deque(maxlen=10000) # double-ended queue; acts like list, but elements can be added/removed from either end
+        self.gamma = 0.95 # decay or discount rate: enables agent to take into account future actions in addition to the immediate ones, but discounted at this rate
+        self.epsilon = epsilon # exploration rate: how much to act randomly; more initially than later due to epsilon decay
+        self.epsilon_decay = 0.995 # decrease number of random explorations as the agent's performance (hopefully) improves over time
+        self.epsilon_min = 0.01 # minimum amount of random exploration permitted
+        self.learning_rate = 0.00001 # rate at which NN adjusts models parameters via SGD to reduce cost 
+        self.model = self._build_model() # private method 
+    
+    def _build_model(self):
+        # neural net to approximate Q-value function:
+        model = Sequential()
+        model.add(Dense(100, input_dim=self.state_size, activation='relu')) # 1st hidden layer; states as input
+        model.add(Dense(200, activation='relu')) # 2nd hidden layer
+        model.add(Dense(200, activation='relu')) # 2nd hidden layer
+        model.add(Dense(self.action_size, activation='linear')) # 2 actions, so 2 output neurons: 0 and 1 (L/R)
+        model.compile(loss='mse',
+                      optimizer=Adam(lr=self.learning_rate))
+        return model
+    
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done)) # list of previous experiences, enabling re-training later
+
+    def act(self, state):
+        if np.random.rand() <= self.epsilon: # if acting randomly, take random action
+            return random.randrange(self.action_size)
+        act_values = self.model.predict(state) # if not acting randomly, predict reward value based on current state
+        return np.argmax(act_values[0]) # pick the action that will give the highest reward (i.e., go left or right?)
+
+    def replay(self, batch_size): # method that trains NN with experiences sampled from memory
+        minibatch = random.sample(self.memory, batch_size) # sample a minibatch from memory
+        for state, action, reward, next_state, done in minibatch: # extract data for each minibatch sample
+            target = reward # if done (boolean whether game ended or not, i.e., whether final state or not), then target = reward
+            if not done: # if not done, then predict future discounted reward
+                target = (reward + self.gamma * # (target) = reward + (discount rate gamma) * 
+                          np.amax(self.model.predict(next_state)[0])) # (maximum target Q based on future action a')
+            target_f = self.model.predict(state) # approximately map current state to future discounted reward
+            target_f[0][action] = target
+            self.model.fit(state, target_f, epochs=1, verbose=0) # single epoch of training with x=state, y=target_f; fit decreases loss btwn target_f and y_hat
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+    def load(self, name):
+        self.model.load_weights(name)
+
+    def save(self, name):
+        self.model.save_weights(name)
+
