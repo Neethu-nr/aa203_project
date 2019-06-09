c5560cacde187bcbae05d194a9b1c174f96efc54
AlbinJagesten <Albin.Jagesten@gmail.com>
2019-06-08 17:07:17 -0700

    Cleanup

--- Simulation/run_simulation.m
+++ Simulation/run_simulation.m
@@ -10,9 +10,12 @@ global T;T=res.T;
 dt = T/N;
 
 [x,y,h,V,gamma,chi,alpha,mu] = varToState(res.var);
-
 u = [alpha';mu'];
+
+%[alpha,mu] = optimal_control(dt);
+%u = [alpha;mu];
+
 x0 = [x(1) y(1) h(1) V(1) gamma(1) chi(1)];
 
-airplane_simulator(@aircraft_dynamics,0.01,u,x0,dt)
+airplane_simulator(@aircraft_dynamics,0.03,u,x0,dt)
 plot3(x,y,h,'g')
